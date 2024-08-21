7c08c20b4f23add05e0e9d77666a9d48e2f3effc
Muipol <mikhail.shashev144@gmail.com>
2024-08-21 20:27:40 +0700

    Выполнил

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Name1", "LastName1", (byte) 21);
+        userService.saveUser("Name2", "LastName2", (byte) 22);
+        userService.saveUser("Name3", "LastName3", (byte) 23);
+        userService.saveUser("Name4", "LastName4", (byte) 24);
+
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+    private final static String URL = "jdbc:mysql://localhost:3306/mydb";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        } catch (ClassNotFoundException
+                | NoSuchMethodException
+                | InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException e) {
+            System.out.println("Не удалось загрузить драйвер mysql");
+        }
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Подключение к базе данных успешно");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Не удалось подключиться к базе данных");
+        }
+        return null;
+    }
 }
